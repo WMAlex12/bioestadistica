f54627be6bc112b6fbf23f4598b141cec56da858
WMAlex <karel-.-alex@hotmail.com>
2024-08-15 10:49:38 -0600

    Introduccion al lenguaje 15-08

---
+++ Primer_Script.R
@@ -0,0 +1,31 @@
+#################
+# Primer Script #
+# Karel Alexis  #
+#################
+
+
+##### Introduccion a R #######
+## Citando a R ###
+citation()
+citation("datasets") ## Citar paquetes en particular
+
+## Ayudas ##
+help() ## Ayuda en general del programa
+help (aov) ## Ayuda en particular de un paquete 
+?aov
+example("aov") ## Crea un ejemplo de la paqueteria
+
+## Crear Objetos/Variables ## 
+## Objetos por String 
+name <- "karel"
+name
+## Objetos numericos
+value1 <- 1
+value1
+value2 <- 2.1 
+value2 
+#Objetos booleanos
+Verdadero <- TRUE 
+Verdadero 
+Falso <- FALSE 
+Falso
